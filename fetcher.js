4ad205433fe8552312b99e442600e90363cb36fe
Ryan Williams <bigwookie3000@gmail.com>
2020-09-24 19:14:35 +0000

    assignment completed

---
+++ fetcher.js
@@ -0,0 +1,19 @@
+let userInput = process.argv.slice(2); 
+const urlAddress = userInput[0]; 
+const filePath = userInput[1]; 
+const fs = require('fs'); 
+const request = require('request');
+
+if (filePath) {
+  request(urlAddress, function (error, response, body) {
+    if (error || response.statusCode !== 200) {
+      console.log("There is an error");
+      return error; 
+    }
+    fs.writeFile(filePath, body, function (err) {
+      if (err) throw err; {
+      console.log(`Downloaded and saved ${body.length} bytes to ${filePath}`);
+      }
+    }
+  )}
+)};
